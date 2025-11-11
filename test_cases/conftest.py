c6e9f66cec6827db8a2549ce8ea29141860eace0
Laraib-Khalid <laraibkhalid7861@gmail.com>
2025-11-11 18:05:33 +0500

    Changing environment information of HTML Report.

--- test_cases/conftest.py
+++ test_cases/conftest.py
@@ -1,4 +1,5 @@
 import pytest
+from pytest_metadata.plugin import metadata_key
 from utilities.driver import create_driver
 
 
@@ -17,3 +18,23 @@ def setup(browser):
     """Run tests on Chrome, Firefox, or Edge."""
     driver = create_driver(browser)
     return driver
+
+
+# ************* For Pytest HTML Reports ****************
+# Hook for adding environment information in HTML report
+
+def pytest_configure(config):
+    config.stash[metadata_key]['Project Name'] = 'Ecommerce Project, nopCommerce'
+    config.stash[metadata_key]['Module Name'] = 'Admin Login Tests'
+    config.stash[metadata_key]['Tester'] = 'Laraib Khalid'
+
+
+
+# Hook for delete/modify environment information in HTML report
+@pytest.mark.optionalhook
+def pytest_metadata(metadata):
+    # metadata.pop('Python', None)
+    # metadata.pop('Platform', None)
+    # metadata.pop('Packages', None)
+    metadata.pop('Plugins', None)
+    metadata.pop('JAVA_HOME', None)
